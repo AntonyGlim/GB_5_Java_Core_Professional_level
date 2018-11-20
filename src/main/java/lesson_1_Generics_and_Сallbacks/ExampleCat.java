7367cb58e51c9af1fa7f865a92d04b590bd22e17
AntonyGlim <Antony.Glim@gmail.com>
2018-11-20 23:02:54 +0300

    Созданы массивы и класс ExampleCat для примера

---
+++ src/main/java/lesson_1_Generics_and_Сallbacks/ExampleCat.java
@@ -0,0 +1,10 @@
+package lesson_1_Generics_and_Сallbacks;
+
+public class ExampleCat {
+
+    private String catName;
+
+    public ExampleCat(String catName) {
+        this.catName = catName;
+    }
+}
--- src/main/java/lesson_1_Generics_and_Сallbacks/ExchangeElementsTask.java
+++ src/main/java/lesson_1_Generics_and_Сallbacks/ExchangeElementsTask.java
@@ -10,5 +10,14 @@ public class ExchangeElementsTask {
         String[] strArr = {"1", "2", "3", "4", "5"};
         Integer[] intArr = {1, 2, 3, 4, 5};
         Float[] fltArr = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f};
+        ExampleCat[] catArr = {
+                new ExampleCat("Мурзик"),
+                new ExampleCat("Барсик"),
+                new ExampleCat("Васька"),
+                new ExampleCat("Рыжик"),
+                new ExampleCat("Снежок"),
+        };
+
+
     }
 }
