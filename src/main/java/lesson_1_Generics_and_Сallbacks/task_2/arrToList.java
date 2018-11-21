0ee29ea4481903cb38e79f313ec61b0ad2c5d14e
AntonyGlim <Antony.Glim@gmail.com>
2018-11-21 10:45:15 +0300

    Написан class arrToList <T>

--- src/main/java/lesson_1_Generics_and_Сallbacks/task_2/arrToList.java
+++ src/main/java/lesson_1_Generics_and_Сallbacks/task_2/arrToList.java
@@ -1,5 +1,14 @@
 package lesson_1_Generics_and_Сallbacks.task_2;
 
-public class arrToList {
+import java.util.ArrayList;
 
+public class arrToList <T> {
+
+    public ArrayList<T> arrToList(T[] arr) {
+        ArrayList<T> list = new ArrayList<T>();
+        for (int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
+        }
+    return list;
+    }
 }
