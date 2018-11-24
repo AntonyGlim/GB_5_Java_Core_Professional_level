127293f0a8ab7a207eac47c28b56bfeb413ba4f3
AntonyGlim <Antony.Glim@gmail.com>
2018-11-24 16:03:15 +0300

    Импортированы JDBC и создана структура приложения

---
+++ src/main/java/lesson_2_Work_with_DataBase/Main.java
@@ -0,0 +1,7 @@
+package lesson_2_Work_with_DataBase;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/lesson_2_Work_with_DataBase/WorkWithDB.java
@@ -0,0 +1,14 @@
+package lesson_2_Work_with_DataBase;
+
+public class WorkWithDB {
+    //TODO - connect();
+    //TODO - createDB() if not exist
+    //TODO - createTable() if not exist
+    //TODO - cleanTable()
+    //TODO - insertIntoTable() data
+    //TODO - int returnCostByName(String productName) from database
+    //TODO - int updateCostByName(String productName) from database
+    //TODO - returnFromDiapason(String columnName, int lowerBorder, int upperBorder) from database
+    //TODO - disconnect();
+
+}
