b477ef5a5939d99c43b3d54a01bc5c0d602e2bab
AntonyGlim <Antony.Glim@gmail.com>
2018-11-24 16:25:19 +0300

    Добавлен метод public static void connect()

--- src/main/java/lesson_2_Work_with_DataBase/WorkWithDB.java
+++ src/main/java/lesson_2_Work_with_DataBase/WorkWithDB.java
@@ -1,7 +1,22 @@
 package lesson_2_Work_with_DataBase;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class WorkWithDB {
+    private static Connection connection;
+    private static Statement statement;
+
     //TODO - connect();
+    public static void connect() throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc:sqlite:src/main/java/lesson_2_Work_with_DataBase/lesson_2_products.db");
+        statement = connection.createStatement();
+    }
+
+
     //TODO - createDB() if not exist
     //TODO - createTable() if not exist
     //TODO - cleanTable()
@@ -10,5 +25,4 @@ public class WorkWithDB {
     //TODO - int updateCostByName(String productName) from database
     //TODO - returnFromDiapason(String columnName, int lowerBorder, int upperBorder) from database
     //TODO - disconnect();
-
 }
