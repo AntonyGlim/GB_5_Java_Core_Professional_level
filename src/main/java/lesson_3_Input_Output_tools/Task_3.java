3bbd4283174f0449d9e7c1f5589da2c8658f19a2
AntonyGlim <Antony.Glim@gmail.com>
2018-11-30 22:40:52 +0300

    Попробуем заменить массив стрингов на массив чаров

--- src/main/java/lesson_3_Input_Output_tools/Task_3.java
+++ src/main/java/lesson_3_Input_Output_tools/Task_3.java
@@ -40,9 +40,8 @@ public class Task_3 {
         }
 
         System.out.println(list.size());
-        System.out.println(list.get(0));
-        System.out.println(list.get(1));
-        System.out.println(list.get(2));
+        
+
     }
 
 
