cd997fdf1594b304661b1db6ef70db9c27dc2ece
AntonyGlim <Antony.Glim@gmail.com>
2018-11-30 12:22:19 +0300

    Пустое задание 3

--- src/main/java/lesson_3_Input_Output_tools/Task_3.java
+++ src/main/java/lesson_3_Input_Output_tools/Task_3.java
@@ -1,3 +1,8 @@
+/**
+ * 3. Написать консольное приложение, которое умеет постранично читать текстовые файлы (размером > 10 mb).
+ * Вводим страницу (за страницу можно принять 1800 символов), программа выводит ее в консоль.
+ * Контролируем время выполнения: программа не должна загружаться дольше 10 секунд, а чтение – занимать свыше 5 секунд.
+ */
 package lesson_3_Input_Output_tools;
 
 import java.io.*;
@@ -12,88 +17,11 @@ import static lesson_3_Input_Output_tools.Task_1.printByteArr;
 //TODO Засечь время выполнения
 //TODO Оптимизировать задания 1 и 2
 public class Task_3 {
-    final static String pathAndName_1 = "src/main/java/lesson_3_Input_Output_tools/vi/01.htm";
-    final static String pathAndName_2 = "src/main/java/lesson_3_Input_Output_tools/vi/02.htm";
-    final static String pathAndName_3 = "src/main/java/lesson_3_Input_Output_tools/vi/03.htm";
-    final static String pathAndName_4 = "src/main/java/lesson_3_Input_Output_tools/vi/04.htm";
-    final static String pathAndName_5 = "src/main/java/lesson_3_Input_Output_tools/vi/05.htm";
-    final static String pathAndName_6 = "src/main/java/lesson_3_Input_Output_tools/vi/06.htm";
-    final static String pathAndName_7 = "src/main/java/lesson_3_Input_Output_tools/vi/07.htm";
-    final static String pathAndName_8 = "src/main/java/lesson_3_Input_Output_tools/vi/08.htm";
-    final static String pathAndName_9 = "src/main/java/lesson_3_Input_Output_tools/vi/09.htm";
-    final static String pathAndName_10 = "src/main/java/lesson_3_Input_Output_tools/vi/10.htm";
-    final static String pathAndName_11 = "src/main/java/lesson_3_Input_Output_tools/vi/result.txt";
 
-    final static int arrSize = 18000;
+    final static int symbolsOnPage = 18000;
+
 
     public static void main(String[] args) throws IOException {
 
-        ArrayList<InputStream> ali = new ArrayList<InputStream>();
-        try {
-            ali.add(new FileInputStream(pathAndName_1));
-            ali.add(new FileInputStream(pathAndName_2));
-            ali.add(new FileInputStream(pathAndName_3));
-            ali.add(new FileInputStream(pathAndName_4));
-            ali.add(new FileInputStream(pathAndName_5));
-            ali.add(new FileInputStream(pathAndName_6));
-            ali.add(new FileInputStream(pathAndName_7));
-            ali.add(new FileInputStream(pathAndName_8));
-            ali.add(new FileInputStream(pathAndName_9));
-            ali.add(new FileInputStream(pathAndName_10));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        SequenceInputStream sqis = new SequenceInputStream(Collections.enumeration(ali));
-        int x;
-        int count = 0;
-        while ((x = sqis.read()) != -1){
-            System.out.print((char) x);
-            count ++;
-            if (count % 200 == 0 ){
-                System.out.println();
-            }
-        }
-        System.out.println("Всего прочитано байт: " + count);
-        sqis.close();
-
-
-
-//        FileInputStream fis = null;
-//        try {
-//            byte[] secondArr = new byte[arrSize];
-//            fis = new FileInputStream(pathAndName);
-//            int count = fis.read(secondArr);
-//            System.out.println("Размер массива: " + count);
-//            printByteArr(secondArr);
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                fis.close();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-
-    public static void writeInFile (String pathAndName_11){
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(pathAndName_11);   //Файл создасться, если до этого не существовал
-            fos.write(byteArr);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                fos.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
