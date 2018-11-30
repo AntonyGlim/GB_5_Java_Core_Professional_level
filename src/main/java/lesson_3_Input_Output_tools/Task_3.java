f8cb216f88daac6ee8d27d12389b926590416c87
AntonyGlim <Antony.Glim@gmail.com>
2018-12-01 00:27:11 +0300

    Добавлены Комментарии в 3 задание. Работают все пункты задания

--- src/main/java/lesson_3_Input_Output_tools/Task_3.java
+++ src/main/java/lesson_3_Input_Output_tools/Task_3.java
@@ -3,6 +3,9 @@
  * Вводим страницу (за страницу можно принять 1800 символов), программа выводит ее в консоль.
  * Контролируем время выполнения: программа не должна загружаться дольше 10 секунд, а чтение – занимать свыше 5 секунд.
  */
+
+//TODO Решить до конца проблему с кодировкой
+
 package lesson_3_Input_Output_tools;
 
 import java.io.*;
@@ -22,12 +25,12 @@ public class Task_3 {
 
     public static void main(String[] args) throws IOException {
 
-        long start = System.currentTimeMillis();
-        ArrayList list = readFromFileWriteToList();
-        System.out.println("Файл считан за: " + (System.currentTimeMillis() - start) + "мс");
+        long start = System.currentTimeMillis();                                                //Засекаем время
+        ArrayList list = readFromFileWriteToList();                                             //Переписываем данные из файла в ArrayList
+        System.out.println("Файл считан за: " + (System.currentTimeMillis() - start) + "мс");   //Выводим затраченное время
 
-        System.out.println("В файле всего: " + list.size() + " страниц.");
-        workingWithUser(readFromFileWriteToList());
+        System.out.println("В файле всего: " + list.size() + " страниц.");                      //Информируем о количестве страниц
+        workingWithUser(readFromFileWriteToList());                                             //В этом блоке пользоваель вводит номера страниц и получает их отображение в консоль
 
     }
 
@@ -37,25 +40,25 @@ public class Task_3 {
      * @throws IOException
      */
     public static ArrayList<String> readFromFileWriteToList() throws IOException {
-        InputStream in = new BufferedInputStream(new FileInputStream(pathAndName));
+        InputStream in = new BufferedInputStream(new FileInputStream(pathAndName));     //BufferedInputStream значительно снижает время на считывание
         ArrayList<String> list = new ArrayList<String>();
         int x;
         int count = 1;
-        char[] chars = new char[symbolsOnPage];
-        while((x = in.read()) != -1) {
-            chars[count - 1] = (char) x;
+        char[] chars = new char[symbolsOnPage];                                         //Длина масива задана в условии
+        while((x = in.read()) != -1) {                                                  //Пока есть следующий символ
+            chars[count - 1] = (char) x;                                                //Пишем его в массив чаров
             if (count == symbolsOnPage){
-                list.add(String.valueOf(chars));
+                list.add(String.valueOf(chars));                                        //Преобразовываем массив чаров в 1 большую строку
                 count = 1;
             } else {
                 count++;
             }
         }
-        char[] ch = new char[count - 1];
+        char[] ch = new char[count - 1];                                                //В этом блоке учитываем остаточек (не полный массив) концовку файла
         for (int i = 0; i < ch.length; i++) {
             ch[i] = chars[i];
         }
-        list.add(String.valueOf(ch));
+        list.add(String.valueOf(ch));                                                   //Добавляем концовку в массив
         in.close();
         return list;
     }
@@ -70,13 +73,13 @@ public class Task_3 {
         while (true){
             System.out.println("Введите номер страницы: (Номер должен быть числом)");
             String s = scanner.nextLine();
-            if (s.equalsIgnoreCase("/q")){                                                          //Ветка для выхода из программы
+            if (s.equalsIgnoreCase("/q")){                                              //Ветка для выхода из программы
                 break;
             }
-            if (isValidNumber(s)){
+            if (isValidNumber(s)){                                                      //Если число (пару коротких проверок)
                 int pageNumber = Integer.parseInt(s);
-                long start = System.currentTimeMillis();
-                if (pageNumber <= list.size() && pageNumber != 0){
+                if (pageNumber <= list.size() && pageNumber != 0){                      //Находится в диапазоне страниц?
+                    long start = System.currentTimeMillis();                            //Засекаем время
                     System.out.println("PAGE " + pageNumber + " START:");
                     System.out.println(list.get(pageNumber - 1));
                     System.out.print("** PAGE " + pageNumber + " END ** ");
@@ -92,8 +95,8 @@ public class Task_3 {
 
     /**
      * Метод проверит, является-ли строка введенная пользователем положительным числом
-     * @param str
-     * @return
+     * @param str символы, которые необходимо проверить
+     * @return - да/нет
      */
     private static boolean isValidNumber(String str){
         Pattern p = Pattern.compile("\\d+");
