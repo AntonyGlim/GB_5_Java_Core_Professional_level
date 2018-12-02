49b2137a57ebdbd6c24a333f8c1f7e9f5e51b592
AntonyGlim <Antony.Glim@gmail.com>
2018-12-02 15:18:19 +0300

    Попробуем переписать как в примере

--- src/main/java/lesson_4_Multithreading_Part_I/Task_1_ABCABC.java
+++ src/main/java/lesson_4_Multithreading_Part_I/Task_1_ABCABC.java
@@ -1,3 +1,7 @@
+/**
+ * 1. Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз (порядок – ABСABСABС).
+ * Используйте wait/notify/notifyAll.
+ */
 package lesson_4_Multithreading_Part_I;
 
 public class Task_1_ABCABC {
@@ -6,8 +10,15 @@ public class Task_1_ABCABC {
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 5; i++) {
-                    System.out.print("A");
+                synchronized (this){
+                    for (int i = 0; i < 5; i++) {
+                        System.out.print("A");
+                        try {
+                            wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
             }
         });
@@ -15,8 +26,15 @@ public class Task_1_ABCABC {
         Thread t2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 5; i++) {
-                    System.out.print("B");
+                synchronized (this){
+                    for (int i = 0; i < 5; i++) {
+                        System.out.print("B");
+                        try {
+                            wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
             }
         });
@@ -24,8 +42,15 @@ public class Task_1_ABCABC {
         Thread t3 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 5; i++) {
-                    System.out.print("C");
+                synchronized (this){
+                    for (int i = 0; i < 5; i++) {
+                        System.out.print("C");
+                        try {
+                            wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
             }
         });
