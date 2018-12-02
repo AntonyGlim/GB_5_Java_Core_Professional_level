00de5916f9c07fbb22acdd415f3449342aa15a0c
AntonyGlim <Antony.Glim@gmail.com>
2018-12-02 14:27:04 +0300

    Написан метод, который выводит символ А

--- src/main/java/lesson_4_Multithreading_Part_I/Task_1_ABCABC.java
+++ src/main/java/lesson_4_Multithreading_Part_I/Task_1_ABCABC.java
@@ -1,4 +1,15 @@
 package lesson_4_Multithreading_Part_I;
 
 public class Task_1_ABCABC {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.print("A");
+                }
+            }
+        });
+        t1.start();
+    }
 }
