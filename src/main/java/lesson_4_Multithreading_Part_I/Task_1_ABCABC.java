a0acd8a2f790bdf0f9ccd2f97e6afbe4a128866e
AntonyGlim <Antony.Glim@gmail.com>
2018-12-02 15:46:43 +0300

    задача решена костыльно слипами

--- src/main/java/lesson_4_Multithreading_Part_I/Task_1_ABCABC.java
+++ src/main/java/lesson_4_Multithreading_Part_I/Task_1_ABCABC.java
@@ -8,9 +8,9 @@ public class Task_1_ABCABC {
     public static void main(String[] args) {
 
         Symbol symbol = new Symbol();
-        Thread t1 = new Thread(new PrintSymbol(symbol, "A"));
-        Thread t2 = new Thread(new PrintSymbol(symbol, "B"));
-        Thread t3 = new Thread(new PrintSymbol(symbol, "C"));
+        Thread t1 = new Thread(new PrintSymbol(symbol, "A", 0));
+        Thread t2 = new Thread(new PrintSymbol(symbol, "B", 100));
+        Thread t3 = new Thread(new PrintSymbol(symbol, "C", 200));
         t1.start();
         t2.start();
         t3.start();
@@ -30,19 +30,29 @@ class Symbol{
 class PrintSymbol implements Runnable{
     Symbol symb;
     String word;
+    long sleepTime;
 
-    public PrintSymbol(Symbol symb, String word) {
+    public PrintSymbol(Symbol symb, String word, long sleepTime) {
         this.symb = symb;
         this.word = word;
+        this.sleepTime = sleepTime;
     }
 
     @Override
     public void run() {
-        synchronized (symb) {
             symb.symbol = word;
+        try {
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
             for (int i = 0; i < 5; i++) {
                 System.out.printf("%s", /*Thread.currentThread().getName(),*/ word);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-        }
     }
 }
