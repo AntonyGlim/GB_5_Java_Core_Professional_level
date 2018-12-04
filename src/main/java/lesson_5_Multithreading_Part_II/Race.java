9b307cd3eedf2cfb9e0316f2f0b1fefa038221e4
AntonyGlim <Antony.Glim@gmail.com>
2018-12-04 23:06:15 +0300

    Заполнены заданием, которое необходимо модифицировать

--- src/main/java/lesson_5_Multithreading_Part_II/Race.java
+++ src/main/java/lesson_5_Multithreading_Part_II/Race.java
@@ -1,4 +1,12 @@
 package lesson_5_Multithreading_Part_II;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Race {
+    private ArrayList<Stage> stages;
+    public ArrayList<Stage> getStages() { return stages; }
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<Stage>(Arrays.asList(stages));
+    }
 }
--- src/main/java/lesson_5_Multithreading_Part_II/Stage.java
+++ src/main/java/lesson_5_Multithreading_Part_II/Stage.java
@@ -1,4 +1,10 @@
 package lesson_5_Multithreading_Part_II;
 
-public class Stage {
+public abstract class Stage {
+    protected int length;
+    protected String description;
+    public String getDescription() {
+        return description;
+    }
+    public abstract void go(Car c);
 }
