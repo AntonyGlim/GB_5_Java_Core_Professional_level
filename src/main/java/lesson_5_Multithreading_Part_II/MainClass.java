774e8e86c3b6b5ad98de3f3d1a4ea59be3fcedb4
AntonyGlim <Antony.Glim@gmail.com>
2018-12-05 22:39:19 +0300

    Добавлен CyclicBarier на этапе подготовки

--- src/main/java/lesson_5_Multithreading_Part_II/MainClass.java
+++ src/main/java/lesson_5_Multithreading_Part_II/MainClass.java
@@ -10,15 +10,17 @@ package lesson_5_Multithreading_Part_II;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.CyclicBarrier;
 
 public class MainClass {
     public static final int CARS_COUNT = 4;
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        CyclicBarrier cb = new CyclicBarrier(CARS_COUNT);
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cb);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
