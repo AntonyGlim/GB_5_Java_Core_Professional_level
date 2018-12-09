55baa77c4957301e222511f6739f81ea0c823064
AntonyGlim <Antony.Glim@gmail.com>
2018-12-09 12:59:51 +0300

    Добавлен метод добавления записей в таблицу

--- src/main/java/lesson_6_Tests_jUnit_Logging_Log4о/workWithDB.java
+++ src/main/java/lesson_6_Tests_jUnit_Logging_Log4о/workWithDB.java
@@ -6,6 +6,25 @@
  */
 package lesson_6_Tests_jUnit_Logging_Log4о;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class workWithDB {
+
+    /**
+     * Метод добавляет запись в таблицу
+     * @param statement - объект для подключения к БД
+     * @param table_name - имя таблицы
+     * @param second_name - фамилия студента
+     * @param score - оценка студента
+     * @throws SQLException
+     */
+    public void insertIntoTable(Statement statement, String table_name, String second_name, int score) throws SQLException {
+        String sql = String.format("INSERT INTO %s (prodid, title, cost) " +
+                "VALUES ('%s', %d);", table_name, second_name, score);
+        statement.execute(sql);
+    }
+
+    
 
 }
--- src/test/java/lesson_6_Tests/workWithDBTest.java
+++ src/test/java/lesson_6_Tests/workWithDBTest.java
@@ -1,5 +1,41 @@
 package lesson_6_Tests;
 
+import org.junit.After;
+import org.junit.Before;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class workWithDBTest {
+    private static Connection connection;
+    private static Statement statement;
+    private static String tableName = "stud";
+
+    @Before
+    /**
+     * Устанавливаем соединение с БД
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public static void connect() throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc:sqlite:src/main/java/lesson_6_Tests_jUnit_Logging_Log4о/lesson_6_students.db");
+        statement = connection.createStatement();
+    }
+
+    @After
+    /**
+     * Закрываем соединение с БД
+     */
+    public static void disconnect(){
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
