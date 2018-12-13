eaaf8a4a3154e56b077b334d1d37858977b94047
AntonyGlim <Antony.Glim@gmail.com>
2018-12-13 23:16:32 +0300

    Добавлен метод startTest

--- src/main/java/lesson_7_Reflection_API_Annotation/MyPersonalTestClass.java
+++ src/main/java/lesson_7_Reflection_API_Annotation/MyPersonalTestClass.java
@@ -9,22 +9,43 @@ import java.lang.reflect.Method;
 
 public class MyPersonalTestClass {
 
-    public static void main(String[] args) throws IllegalAccessException, InstantiationException, InvocationTargetException {
 
+    public static void main(String[] args) {
+
+        try {
+            startTest();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void startTest() throws IllegalAccessException, InstantiationException, InvocationTargetException {
+
+        //Создаем экземпляр класса без ключевого слова new
         Class myTests1Class = MyTests1.class;
         MyTests1 mt1 = (MyTests1) myTests1Class.newInstance();
-        Method[] methods = MyTests1.class.getDeclaredMethods();
+
+        //Эти флаги послужат для последовательного запуска методов класса
         Method beforeSuiteMethod = null;
         Method myTestMethod = null;
         Method afterSuiteMethod = null;
+
+        //Эти флаги послужат для определения - единажды-ли встречается метод с определенной анотацией
         boolean isBeforeSuiteMeetsOnce = true;
         boolean isAfterSuiteMeetsOnce = true;
+
+        //Извлекаем все методы,которые содержаться в классе
+        Method[] methods = MyTests1.class.getDeclaredMethods();
         for (Method o : methods) {
-            if(o.getAnnotation(BeforeSuite.class) != null) {
-                if (isBeforeSuiteMeetsOnce){
+            if(o.getAnnotation(BeforeSuite.class) != null) {        //Если соответствует определенной анотации
+                if (isBeforeSuiteMeetsOnce){                        //Если еще не встречался
                     beforeSuiteMethod = o;
                     isBeforeSuiteMeetsOnce = false;
-                } else {
+                } else {                                            //Иначе выкидываем исключение
                     throw new RuntimeException("Метод с анотацией @BeforeSuite должен быть только один!");
                 }
             }
@@ -41,6 +62,8 @@ public class MyPersonalTestClass {
 
             }
         }
+
+        //Исполняем указанные методы
         beforeSuiteMethod.invoke(mt1);
         myTestMethod.invoke(mt1);
         afterSuiteMethod.invoke(mt1);
