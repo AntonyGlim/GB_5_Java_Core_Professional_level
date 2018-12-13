34cc8d40cf53b84df0b82fd7654b94e782f65e11
AntonyGlim <Antony.Glim@gmail.com>
2018-12-13 21:28:12 +0300

    В анотации добавлены нужные описания

--- src/main/java/lesson_7_Reflection_API_Annotation/MyTests1.java
+++ src/main/java/lesson_7_Reflection_API_Annotation/MyTests1.java
@@ -1,6 +1,8 @@
 package lesson_7_Reflection_API_Annotation;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
+import lesson_7_Reflection_API_Annotation.myAnotation.AfterSuite;
+import lesson_7_Reflection_API_Annotation.myAnotation.BeforeSuite;
+import lesson_7_Reflection_API_Annotation.myAnotation.MyTest;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -27,13 +29,13 @@ public class MyTests1 {
 
     }
 
-//    @BeforeSuite
+    @BeforeSuite
     public static void firstOfAll(){
         ExampleForMyPersonalTestClass example = new ExampleForMyPersonalTestClass();
         exampleClass = example.getClass();
     }
 
-//    @AfterSuite
+    @AfterSuite
     public static void afterAll(){
 
     }
@@ -44,7 +46,7 @@ public class MyTests1 {
      * @throws InvocationTargetException
      * @throws IllegalAccessException
      */
-//    @MyTest
+    @MyTest
     public static void myTest1() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Class[] paramTypes = new Class[] { int.class, int.class };
         Method method = exampleClass.getDeclaredMethod("plus", paramTypes);
