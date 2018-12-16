38e767b5e5677bef3905484de62d51baeceb2ae9
AntonyGlim <Antony.Glim@gmail.com>
2018-12-16 10:24:30 +0300

    Добавлено задание

--- src/main/java/lesson_8_Interview_Spring_ORM/job_interview_task/SnakeArray.java
+++ src/main/java/lesson_8_Interview_Spring_ORM/job_interview_task/SnakeArray.java
@@ -1,4 +1,12 @@
+/**
+ * 1 Задать размерность матрицы от 5 до 7
+ * 2 Создать матрицу с указанной размерностью
+ * 3 Заполнить матрицу по спирали (числа увеличиваются на 1) (число начинается с 1)
+ */
 package lesson_8_Interview_Spring_ORM.job_interview_task;
 
 public class SnakeArray {
+    public static void main(String[] args) {
+        
+    }
 }
