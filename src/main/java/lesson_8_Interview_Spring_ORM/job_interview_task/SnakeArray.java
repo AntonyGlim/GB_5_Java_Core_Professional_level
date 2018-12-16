80a31b7ddd467dc1c38a579db525a144ddc1d027
AntonyGlim <Antony.Glim@gmail.com>
2018-12-16 12:14:02 +0300

    Добавлены комментарии, задание готово!

--- src/main/java/lesson_8_Interview_Spring_ORM/job_interview_task/SnakeArray.java
+++ src/main/java/lesson_8_Interview_Spring_ORM/job_interview_task/SnakeArray.java
@@ -43,7 +43,7 @@ public class SnakeArray {
     }
 
     /**
-     * Метод выведет на экран двумерный массив
+     * Метод выведет на экран двумерный массив (с форматированием)
      * @param arr
      */
     private static void printArray(int[][] arr){
@@ -55,20 +55,29 @@ public class SnakeArray {
         }
     }
 
+    /**
+     * Метод заполняет массив по спирали
+     * @param arr - передаем в метод массив
+     * @param maxNumberInArr - максимальное значение, которое будет присутствовать в этом массиве
+     * @param size - обределенные ранее размеры массивва
+     * @return - заполненный по спирали массив
+     */
     private static int[][] fillInWithNumbers(int[][] arr, int maxNumberInArr, int size) {
+        //Если пришел массив с 1 элементом
         if(size == 1){
             arr[0][0] = 1;
         }
-        int startLineIndex = 0;
-        int startColumnIndex = 0;
-        int presentValue = 1;
 
-        while (size >= 2){
+        int startLineIndex = 0;                                             //индекс строки с которой начнется заполнение
+        int startColumnIndex = 0;                                           //индекс столбца с которого начнется заполнение
+        int presentValue = 1;                                               //текущее значение с которого начнется заполнение массива
+
+        while (size >= 2){                                                  //пока размеры массива больше или равны 2
             //Работа с первой строкой
             for (int i = startLineIndex; i < size; i++) {
                 arr[startLineIndex][i] = presentValue;
                 presentValue++;
-                if (presentValue > maxNumberInArr)return arr;
+                if (presentValue > maxNumberInArr)return arr;               //если мы дошли до максимального значения в массиве, то завершаем заполнение
             }
             //Работа с последним столбцом
             for (int i = startLineIndex + 1; i < size; i++) {
@@ -88,8 +97,8 @@ public class SnakeArray {
                 presentValue++;
                 if (presentValue > maxNumberInArr)return arr;
             }
-            size -= 1;
-            startLineIndex++;
+            size -= 1;                                                      //уменьшаем значение размера на 1
+            startLineIndex++;                                               //изменяем стартовые индексы для следующей итерации
             startColumnIndex++;
         }
 
