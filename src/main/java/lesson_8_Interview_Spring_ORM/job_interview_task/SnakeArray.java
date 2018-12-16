4bb02165d4b7d8690e29f77f393f5abc2d9cb7fb
AntonyGlim <Antony.Glim@gmail.com>
2018-12-16 10:42:25 +0300

    Расчитана сторона массива calculateSquareSideSize

--- src/main/java/lesson_8_Interview_Spring_ORM/job_interview_task/SnakeArray.java
+++ src/main/java/lesson_8_Interview_Spring_ORM/job_interview_task/SnakeArray.java
@@ -5,8 +5,31 @@
  */
 package lesson_8_Interview_Spring_ORM.job_interview_task;
 
+import java.util.Scanner;
+
 public class SnakeArray {
+
+    static private int maxNumberInArr = 0;
+    static private int squareSideSize = 0;
+
     public static void main(String[] args) {
-        
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Введите последнее число последовательности...");
+            maxNumberInArr = sc.nextInt();
+            System.out.println(calculateSquareSideSize(maxNumberInArr));
+        }
+    }
+
+    private static int calculateSquareSideSize(int maxNumberInArr){
+        double sqrDoub = Math.sqrt(maxNumberInArr);
+        int sqrInt = (int) Math.sqrt(maxNumberInArr);
+        int result;
+        if (sqrDoub % sqrInt != 0){
+            result = sqrInt + 1;
+        } else {
+            result = sqrInt;
+        }
+        return result;
     }
 }
