63f401fdf9286858f13729e9d0ac83c17a948688
AntonyGlim <Antony.Glim@gmail.com>
2018-12-16 11:18:08 +0300

    Добавлен форматированный вывод

--- src/main/java/lesson_8_Interview_Spring_ORM/job_interview_task/SnakeArray.java
+++ src/main/java/lesson_8_Interview_Spring_ORM/job_interview_task/SnakeArray.java
@@ -49,7 +49,7 @@ public class SnakeArray {
     private static void printArray(int[][] arr){
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + " ");
+                System.out.printf("%6d", arr[i][j]);
             }
             System.out.println();
         }
