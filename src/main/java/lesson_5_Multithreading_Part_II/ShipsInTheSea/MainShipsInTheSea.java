5174bd7225eed09520543539e548f7c2d8479f4e
AntonyGlim <Antony.Glim@gmail.com>
2018-12-18 21:13:32 +0300

    Создан корабль в Main

--- src/main/java/lesson_5_Multithreading_Part_II/ShipsInTheSea/MainShipsInTheSea.java
+++ src/main/java/lesson_5_Multithreading_Part_II/ShipsInTheSea/MainShipsInTheSea.java
@@ -4,5 +4,6 @@ public class MainShipsInTheSea {
     public static void main(String[] args) {
         SeaPort portStart = new SeaPort("Порт Начало", 6000, 5700, 50);
         SeaPort portEnd = new SeaPort("Порт Конец", 6000, 0, 50);
+        Ship ship_1 = new Ship("Победа", 100, 20);
     }
 }
--- src/main/java/lesson_5_Multithreading_Part_II/ShipsInTheSea/Ship.java
+++ src/main/java/lesson_5_Multithreading_Part_II/ShipsInTheSea/Ship.java
@@ -1,4 +1,13 @@
 package lesson_5_Multithreading_Part_II.ShipsInTheSea;
 
 public class Ship {
+    private String shipName;                                            //Название корабля
+    private int shipCapacity;                                           //Вместимость корабля, ед
+    private int shipSpeed;                                              //Скорость движения корабля миль/час
+
+    public Ship(String shipName, int shipCapacity, int shipSpeed) {
+        this.shipName = shipName;
+        this.shipCapacity = shipCapacity;
+        this.shipSpeed = shipSpeed;
+    }
 }
