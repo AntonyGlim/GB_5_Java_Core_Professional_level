bc09915702a8718d38a7d3ee15c43949b54b4ef9
AntonyGlim <Antony.Glim@gmail.com>
2018-12-18 21:08:02 +0300

    В порты добавлена максимальная вместимость. Созданы 2 порта

--- src/main/java/lesson_5_Multithreading_Part_II/ShipsInTheSea/MainShipsInTheSea.java
+++ src/main/java/lesson_5_Multithreading_Part_II/ShipsInTheSea/MainShipsInTheSea.java
@@ -2,6 +2,7 @@ package lesson_5_Multithreading_Part_II.ShipsInTheSea;
 
 public class MainShipsInTheSea {
     public static void main(String[] args) {
-        
+        SeaPort portStart = new SeaPort("Порт Начало", 6000, 5700, 50);
+        SeaPort portEnd = new SeaPort("Порт Конец", 6000, 0, 50);
     }
 }
--- src/main/java/lesson_5_Multithreading_Part_II/ShipsInTheSea/SeaPort.java
+++ src/main/java/lesson_5_Multithreading_Part_II/ShipsInTheSea/SeaPort.java
@@ -1,14 +1,16 @@
 package lesson_5_Multithreading_Part_II.ShipsInTheSea;
 
 public class SeaPort {
-    private int portCapacity;                       //Вместимость порта, ед
+    private int portCapacity;                       //Текущая вместимость порта, ед
+    private int maxPortCapacity;                    //Максимальная вместимость порта, ед
     private int loadingSpeed;                       //Скорость погрузки на корабль ед/с
     private String portName;                        //Название порта
 
-    public SeaPort(int portCapacity, int loadingSpeed, String portName) {
-        this.portCapacity = portCapacity;
-        this.loadingSpeed = loadingSpeed;
+    public SeaPort(String portName,int maxPortCapacity, int portCapacity, int loadingSpeed) {
         this.portName = portName;
+        this.portCapacity = portCapacity;
+        this.maxPortCapacity = maxPortCapacity;
+        this.loadingSpeed = loadingSpeed;
     }
 
 
