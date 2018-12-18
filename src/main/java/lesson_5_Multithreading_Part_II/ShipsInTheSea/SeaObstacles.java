44eb79a58ea5ac4add4995f4179107c3bf419a6b
AntonyGlim <Antony.Glim@gmail.com>
2018-12-18 23:24:28 +0300

    Создан класс для формирования путешествия

--- src/main/java/lesson_5_Multithreading_Part_II/ShipsInTheSea/SeaObstacles.java
+++ src/main/java/lesson_5_Multithreading_Part_II/ShipsInTheSea/SeaObstacles.java
@@ -1,5 +1,20 @@
 package lesson_5_Multithreading_Part_II.ShipsInTheSea;
 
-public class SeaObstacles {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+/**
+ * Класс формирует массив с этапами движения корабля
+ * (используя аргумент переменной длинны)
+ */
+public class SeaObstacles {
+    private ArrayList<SeaAdventure> seaAdventures;
+
+    public ArrayList<SeaAdventure> getSeaAdventures() {
+        return seaAdventures;
+    }
+
+    public SeaObstacles(SeaAdventure ... seaAdventures) {
+        this.seaAdventures = new ArrayList<SeaAdventure>(Arrays.asList(seaAdventures));
+    }
 }
